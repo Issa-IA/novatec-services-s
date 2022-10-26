9227301da01296078508ba68dede8dda7d46c082
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-10-26 11:58:27 +0100

    Update models.py

--- calendarevent/models/models.py
+++ calendarevent/models/models.py
@@ -8,11 +8,17 @@ class   Calendarinherit(models.Model):
     def update_location(self):
         for rec in self:
             if rec.partner_ids:
-                if rec.partner_ids[0].zip and rec.partner_ids[0].city:
-                    rec.location = str(rec.partner_ids[0].zip) + ' ' + str(rec.partner_ids[0].city)
-                if rec.partner_ids[0].zip and not rec.partner_ids[0].city:
+                if rec.partner_ids[0].zip and rec.partner_ids[0].city and rec.partner_ids[0].street:
+                    rec.location =str(rec.partner_ids[0].street)+' '+ str(rec.partner_ids[0].zip) + ' ' + str(rec.partner_ids[0].city)
+                if rec.partner_ids[0].zip and not rec.partner_ids[0].city and not rec.partner_ids[0].street:
                     rec.location = str(rec.partner_ids[0].zip)
-                if not rec.partner_ids[0].zip and rec.partner_ids[0].city:
+                if not rec.partner_ids[0].zip and rec.partner_ids[0].city and not rec.partner_ids[0].street:
                     rec.location = str(rec.partner_ids[0].city)
+                if not rec.partner_ids[0].zip and not rec.partner_ids[0].city and  rec.partner_ids[0].street:
+                    rec.location = str(rec.partner_ids[0].street)
+                if not rec.partner_ids[0].zip and  rec.partner_ids[0].city and  rec.partner_ids[0].street:
+                    rec.location = str(rec.partner_ids[0].street) +' '+ str(rec.partner_ids[0].city)
+                if  rec.partner_ids[0].zip and  not rec.partner_ids[0].city and  rec.partner_ids[0].street:
+                    rec.location = str(rec.partner_ids[0].street) +' '+ str(rec.partner_ids[0].zip)
 
 
