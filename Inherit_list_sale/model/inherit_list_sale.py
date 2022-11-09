224d502678fa5010f51040f8c34d8fc1ae577ff2
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2022-11-09 14:14:46 +0100

    Update inherit_list_sale.py

--- Inherit_list_sale/model/inherit_list_sale.py
+++ Inherit_list_sale/model/inherit_list_sale.py
@@ -7,9 +7,9 @@ class StockHerit(models.Model):
     etiquette_maintenance = fields.Selection([('maintenance', 'Maintenance'), ('autres', 'Autres')], compute='_maintenance_etiquette', store=True)
 
 
-
+    @api.depends('sale_maintnance')
     def _maintenance_etiquette(self):
         for rec in self:
             if rec.sale_maintnance :
-                rec.write({'etiquette_maintenance': "maintenance"})
+                rec.etiquette_maintenance = 'maintenance'
 
